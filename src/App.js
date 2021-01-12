c49caa5a6e12510e4657b80caeb70a3ac69323a5
johan <johan2.github@gmail.com>
2021-01-13 00:11:36 +0100

    image evolution and lost game case

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
+import image1 from './images/pendu1.svg';
+import image2 from './images/pendu2.svg';
+import image3 from './images/pendu3.svg';
+import image4 from './images/pendu4.svg';
+import image5 from './images/pendu5.svg';
+import image6 from './images/pendu6.svg';
+import image7 from './images/pendu7.svg';
+import image8 from './images/pendu8.svg';
+import image9 from './images/pendu9.svg';
+import image10 from './images/pendu10.svg';
+import image11 from './images/pendu11.svg';
 import './App.css';
-import { Component, createRef, useRef } from 'react';
+import { Component } from 'react';
 
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-const phrases = ['test', 'phrase', 'pendu', 'developement', 'reussite', 'escalier', 'balancoire', 'aleatoire', 'eau', 'honteux', 'condamne', 'invitation', 'constante', 'signal', 'équivalent', 'autonomisation', 'mousse', 'commercial', 'conversion', 'elargir', 'banque', 'bijou']
+const phrases = ['test', 'phrase', 'pendu', 'developement', 'reussite', 'escalier', 'balancoire', 'aleatoire', 'eau', 'honteux', 'condamne', 'invitation', 'constante', 'signal', 'equivalent', 'autonomisation', 'mousse', 'commercial', 'conversion', 'elargir', 'banque', 'bijou']
 
 class App extends Component {
 
@@ -13,43 +24,66 @@ class App extends Component {
     this.handleLetterClick = this.handleLetterClick.bind(this)
     this.handleClickRestart = this.handleClickRestart.bind(this)
     this.handlerKeyDown = this.handlerKeyDown.bind(this)
+    this.updateImage = this.updateImage.bind(this)
     this.state = {
         usedLetters: [],
         phrase: phrase,
-        attempts: 0
+        wrongLetters: 0,
+        image: image1
     }
   }
 
   handlerKeyDown (e) {
     e.preventDefault()
-    if(e.keyCode > 64 && e.keyCode < 91) {
-      const letter = String.fromCharCode(e.keyCode)
+    if(e.keyCode >= 65 && e.keyCode <= 90) {
+      const letter = e.key.toLowerCase()
+      const isIncluded = includeLetter(letter, this.state.phrase)
+      const image = isIncluded ? this.state.image : this.updateImage()
       this.setState({
         usedLetters: [...this.state.usedLetters, letter],
         phrase: this.state.phrase,
-        attempts: this.state.attempts + 1
+        wrongLetters: isIncluded ? this.state.wrongLetters : this.state.wrongLetters + 1,
+        image: image
       })
-      const button = createRef()
-      button.className = "btn btn-secondary btn-lg disabled m-1"
     }
   }
 
-  componentDidMount() {
+  componentDidMount () {
     document.addEventListener("keydown", this.handlerKeyDown)
   }
 
-  componentWillUnmount() {
+  componentWillUnmount () {
     document.removeEventListener("keydown", function() {
 
     })
   }
 
+  updateImage () {
+    switch (this.state.image) {
+      case image1: return image2;
+      case image2: return image3;
+      case image3: return image4;
+      case image4: return image5;
+      case image5: return image6;
+      case image6: return image7;
+      case image7: return image8;
+      case image8: return image9;
+      case image9: return image10;
+      case image10: return image11;
+      case image11: return image11;
+      default: return this.state.image
+    }
+  }
+
 
   handleLetterClick (letter) {
+    const isIncluded = includeLetter(letter, this.state.phrase)
+    const image = isIncluded ? this.state.image : this.updateImage()
     this.setState({
       usedLetters: [...this.state.usedLetters, letter],
       phrase: this.state.phrase,
-      attempts: this.state.attempts + 1
+      wrongLetters: isIncluded ? this.state.wrongLetters : this.state.wrongLetters + 1,
+      image: image
     })
   }
 
@@ -58,13 +92,30 @@ class App extends Component {
     this.setState({
       usedLetters: [],
       phrase: phrase,
-      attempts: 0
+      wrongLetters: 0,
+      image: image1
     })
   }
 
   render () {
     const text = computeDisplay(this.state.phrase, this.state.usedLetters)
-    const endGame = text.includes('_') ? false : true
+    const gameWin = text.includes('_') ? false : true
+    const gameLost = this.state.wrongLetters >= 10 ? true : false
+    let elt = <div></div>
+    if(gameWin) {
+      elt = <div>
+        <h1>GAGNÉ</h1>
+        <button className="btn btn-info btn-lg" value="restart" onClick={this.handleClickRestart}>Rejouer</button>
+      </div>
+    } else if(gameLost) {
+      elt = <div>
+        <h1>PERDU</h1>
+        <button className="btn btn-info btn-lg" value="restart" onClick={this.handleClickRestart}>Rejouer</button>
+      </div>
+    } else { 
+      elt = <Buttons onClickButton={this.handleLetterClick}/>
+    }
+
     return (
       <div className="App">
         <header className="App-header mt-4">
@@ -72,16 +123,17 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <div className="title">JEU DU PENDU</div>
           </div>
-          <div className="attempts">Essais : {this.state.attempts}</div>
         </header>
         <div className="main container mt-5">
-          <div className="text-center">
-            <div className="masque p-5">{text}</div>
-            <div className="buttons p-5">
-              {endGame
-              ? <div><h1>GAGNÉ</h1><button className="btn btn-info btn-lg" value="restart" onClick={this.handleClickRestart}>Rejouer</button></div>
-              : <Buttons onClickButton={this.handleLetterClick}/>
-              }
+          <div className="row">
+            <div className="col-lg-4 col-md-12 col-sm-12">
+              <div className="image"><img className="img-fluid" src={this.state.image} alt="pendu decoration"/></div>
+            </div>
+            <div className="col-lg-8 col-md-12 col-sm-12">
+              <div className="text-center">
+                <div className="masque p-5">{text}</div>
+                <div className="buttons p-5">{elt}</div>
+              </div>
             </div>
           </div>
         </div>
@@ -90,10 +142,15 @@ class App extends Component {
   }
 }
 
+
 function computeDisplay (phrase, usedLetters) {
   return phrase.replace(/\w/g, (letter) => (
     usedLetters.includes(letter) ? letter : '_'
   ))
+}
+
+function includeLetter(letter, phrase) {
+  return phrase.includes(letter)
 }
 
 class Buttons extends Component {
